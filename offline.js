7bc2f6e88085c6a9ae119012782584f6beae24b5
MPGames <mpgamesdeveloper@gmail.com>
2019-09-04 22:20:21 -0400

    new update

--- offline.js
+++ offline.js
@@ -1,11 +1,10 @@
 ﻿{
-	"version": 1567635066,
+	"version": 1567649936,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
 		"jquery-2.1.1.min.js",
 		"offlineClient.js",
-		"images/sprite-sheet0.png",
 		"icon-16.png",
 		"icon-32.png",
 		"icon-114.png",
